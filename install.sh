113000e163d683bb83db194db439fe28d431395a
gsbigbear <109757157+gsbigbear@users.noreply.github.com>
2022-07-23 14:46:36 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 cd ~
 git clone https://github.com/gsbigbear/picar-oled.git
 cd picar-oled
+sudo systemctl stop oled.service
 sudo cp oled.service /lib/systemd/system/
 pip3 install Adafruit_SSD1306 Adafruit_BBIO
 sudo systemctl daemon-reload
